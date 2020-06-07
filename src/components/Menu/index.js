d80114e90f5a3f6e9ad51c8931705481d5ee763c
Mateus Henrique <mateushenriquechaves1@gmail.com>
2020-06-07 14:03:07 -0300

    remove link effect

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -5,45 +5,52 @@ import { Button } from '@material-ui/core';
 import Home from '../../assets/svg/home.svg';
 import Star from '../../assets/svg/start.svg';
 
-import { ReactSVG } from 'react-svg'
+import { Link } from 'react-router-dom';
+
+import { ReactSVG } from 'react-svg';
 
 import { Container } from './styles';
 
 export default function Menu() {
     return (
         <Container>
-            <Button
-                variant="outlined"
-                color="default"
-                startIcon={<ReactSVG src={Home} />}
-                style={{
-                    display: 'flex',
-                    justifyContent: 'space-around',
-                    width: 360,
-                    borderRadius: 15,
-                    color: '#FFFFFF',
-                    height: 45
-                }}
-            >
-                Home
-            </Button>
+            <Link to="/" style={{ color: 'inherit', textDecoration: 'inherit' }}>
+                <Button
+                    variant="outlined"
+                    color="default"
+                    startIcon={<ReactSVG src={Home} />}
+                    style={{
+                        display: 'flex',
+                        justifyContent: 'space-around',
+                        width: 360,
+                        borderRadius: 15,
+                        color: '#FFFFFF',
+                        height: 45
+                    }}
+                >
+                    Home
+                </Button>
+            </Link>
 
-            <Button
-                variant="outlined"
-                color="default"
-                startIcon={<ReactSVG src={Star} />}
-                style={{
-                    display: 'flex',
-                    justifyContent: 'space-around',
-                    width: 360,
-                    borderRadius: 15,
-                    color: '#FFFFFF',
-                    height: 45,
-                    marginTop: 25
-                }}
-            >
-                Ranking
-            </Button>
+            <Link to="/ranking" style={{ color: 'inherit', textDecoration: 'inherit' }}>
+                <Button
+                    variant="outlined"
+                    color="default"
+                    startIcon={<ReactSVG src={Star} />}
+                    style={{
+                        display: 'flex',
+                        justifyContent: 'space-around',
+                        width: 360,
+                        borderRadius: 15,
+                        color: '#FFFFFF',
+                        height: 45,
+                        marginTop: 25
+                    }}
+                >
+                    Ranking
+                </Button>
+            </Link>
+
         </Container>
     )
 }
